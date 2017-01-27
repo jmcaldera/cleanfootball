9c392c494ddee838258411320414cac0cec188c8
jmcaldera <manuelcp1992@gmail.com>
2017-01-26 22:34:02 -0400

    Iniciando repositorio CleanFootball

---
+++ app/src/main/java/com/jmcaldera/cleanfootball/CompetitionSelectorActivity.java
@@ -0,0 +1,13 @@
+package com.jmcaldera.cleanfootball;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CompetitionSelectorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_competition_selector);
+    }
+}
